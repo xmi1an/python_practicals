5ce75f27fd3ad7202d6231152d3098804c1adcf1
mi1an <jadavmilan009@gmail.com>
2022-01-03 13:37:34 +0530

    added more practicals

---
+++ practical-5.py
@@ -0,0 +1,10 @@
+# 5. Write a Python Program to print first prime number.
+numr = int(input("Enter range:"))
+
+print("Prime numbers:", end=' ')
+for n in range(1, numr):
+    for i in range(2, n):
+        if(n % i == 0):
+            break
+    else:
+        print(n, end=' ')
